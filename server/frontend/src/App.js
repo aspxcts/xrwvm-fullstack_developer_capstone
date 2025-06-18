2ccea80c2f8beb8a883d3228b91d620be59e14e4
Safir <azamsafir@gmail.com>
2025-06-18 01:53:27 -0400

    module 5 files, kubernates

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -11,8 +11,8 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers/>} />    
-      <Route path="/dealer/:id" element={<Dealer/>} />
-      <Route path="/postreview/:id" element={<PostReview/>} />
+      <Route path="/dealer/:dealer_id" element={<Dealer/>} />
+      <Route path="/postreview/:dealer_id" element={<PostReview/>} />
     </Routes>
   );
 }
